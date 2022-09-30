b66e3f840823b5f1aab238f64c7fb31a51f73f31
PremKumbhar <kumbharprem7738@gmail.com>
2022-09-30 17:47:28 +0530

    [Prem]Added played with single player at position 0

---
+++ src/com/bridgelabz/Snake_and_Ladder.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+public class Snake_and_Ladder {
+	public static void main(String[] args) 
+	{
+		for (int i=0; i <= 100; i++)
+		{
+		  System.out.println("ladder value =" +i);
+		}
+	}
+}
