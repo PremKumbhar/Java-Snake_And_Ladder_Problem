edeafdaf2a17106df9ecd3d493ae7c7e00e45f1b
PremKumbhar <kumbharprem7738@gmail.com>
2022-09-30 18:19:59 +0530

    [Prem]:Added report the number of times the dice was played to win th…

--- src/com/bridgelabz/Snake_and_Ladder.java
+++ src/com/bridgelabz/Snake_and_Ladder.java
@@ -3,12 +3,15 @@ package com.bridgelabz;
 public class Snake_and_Ladder {
 	public static void main(String[] args) 
 	{
-		int i=0;
-		int noPlay=0;
-		int ladder=1;
-		int snake=2;
-			while(i<=100)
+		int i= 0;
+		int noPlay= 0;
+		int ladder= 1;
+		int snake= 2;
+        int count= 0;
+         
+	while(i<=100)
 			{
+	            count=count+1;
 				int roll = (int ) Math.floor((Math.random() * 10) % 3);
 				int value = (int) Math.floor((Math.random() * 10) % 6)+1;
 				System.out.println("Dice Roll =" + value);
@@ -17,6 +20,8 @@ public class Snake_and_Ladder {
 				if(roll == noPlay) 
 				{
 					System.out.println("No Play" + i);
+					System.out.println("counter" + count);
+					
 				}
 				else if (roll == ladder) 
 				{
@@ -25,10 +30,12 @@ public class Snake_and_Ladder {
 			         	{
 				     	i=100;
 					     System.out.println("Ladder" +i);
+					     System.out.println("counter" + count);
 				        }
 			         	else
 			         	{
 					    System.out.println("Ladder" + i);
+					    System.out.println("counter" + count);
 			         	}
 				}
 	    	   else 
@@ -38,13 +45,15 @@ public class Snake_and_Ladder {
 				     {
 				     i=0;
 					       System.out.println("snake" + i);
+					       System.out.println("counter" + count);
 				     }
 				     else
 				     {
 				    	System.out.println("snake" + i);
+				    	System.out.println("counter" + count);
 				     }
 	    	   }
-		
 			}
+
 	}
 }
